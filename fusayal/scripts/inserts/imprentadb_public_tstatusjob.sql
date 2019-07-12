3fb7e45b9e93536f90811b05aeedea56b1f4139e
manuel.japon <manuel.japon@registrocivil.gob.ec>
2019-07-12 16:11:37 -0500

    inserts

---
+++ fusayal/scripts/inserts/imprentadb_public_tstatusjob.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.tstatusjob (sjb_id, sjb_nombre) VALUES (3, 'Incompleto');
+INSERT INTO public.tstatusjob (sjb_id, sjb_nombre) VALUES (5, 'Reimpresion');
+INSERT INTO public.tstatusjob (sjb_id, sjb_nombre) VALUES (2, 'Reportado');
+INSERT INTO public.tstatusjob (sjb_id, sjb_nombre) VALUES (1, 'Inicial');
+INSERT INTO public.tstatusjob (sjb_id, sjb_nombre) VALUES (4, 'Anulado');
+INSERT INTO public.tstatusjob (sjb_id, sjb_nombre) VALUES (6, 'Impreso');
+INSERT INTO public.tstatusjob (sjb_id, sjb_nombre) VALUES (7, 'Elige plantilla');
